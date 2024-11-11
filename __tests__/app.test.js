c6ff936b58c1418161d999be73da44a81fe852e0
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-11 09:01:37 +0000

    add seed to test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,13 @@
+const { data } = require("../db/data/test");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
 describe("", () => {
-  test("", () => {
-  })
-})
+  test("", () => {});
+});
