546a440c29f0087055f74697a1e47fd3a9c46ce7
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-11 09:40:13 +0000

    add createRef function

--- db/seeds/seed-utils.js
+++ db/seeds/seed-utils.js
@@ -1,3 +1,11 @@
 exports.convertDateToTimestamp = (time) => {
   return new Date(time).toISOString();
 };
+
+exports.createRef = (array, key, value) => {
+    return array.reduce((ref, element) => {
+      ref[element[key]] = element[value];
+      return ref;
+    }, {});
+  };
+  
