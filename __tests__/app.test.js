bca1ac8726eebc433bdfbe20ba3fffa317d43d13
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-11 13:59:37 +0000

    refactor endpoint test to async await

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,13 +11,13 @@ afterAll(() => {
   return db.end();
 });
 describe("/api", () => {
-  test("200 GET: Api router returns endpoints", () => {
+  test("200 GET: Api router returns endpoints", async () => {
     const endpointsJSON = require("../endpoints.json");
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJSON);
-      });
+    
+    const {
+      body: { endpoints },
+    } = await request(app).get("/api").expect(200);
+
+    expect(endpoints).toEqual(endpointsJSON);
   });
 });
