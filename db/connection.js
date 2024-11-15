880e515d05b196ebce74008ed6fcf2566cde716d
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-15 17:48:53 +0000

    add changes for hosting databse

--- db/connection.js
+++ db/connection.js
@@ -18,7 +18,10 @@ const config = {
 // if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 //   throw new Error("PGDATABASE or DATABASE_URL not set");
 // }
-
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 
 export default new Pool(config);
