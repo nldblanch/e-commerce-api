366837aba6a5656e111cfd423e4d2412b2dad10b
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-20 17:22:48 +0000

    remove comments

--- db/connection.js
+++ db/connection.js
@@ -1,27 +1,20 @@
-import pkg from "pg"
-const {Pool} = pkg;
-import * as dotenv from "dotenv"
-import path from "path"
+import pkg from "pg";
+const { Pool } = pkg;
+import * as dotenv from "dotenv";
+import path from "path";
 const ENV = process.env.NODE_ENV || "development";
-const dirname = path.resolve()
+const dirname = path.resolve();
 dotenv.config({
-  path: `${dirname}/.env.${ENV}`
+  path: `${dirname}/.env.${ENV}`,
 });
 
 const config = {
   env: ENV,
 };
-// path: `${__dirname}/../.env.${ENV}`,
-// console.log(config)
-// console.log(process.env)
-// console.log(path.resolve())
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//   throw new Error("PGDATABASE or DATABASE_URL not set");
-// }
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-
 
 export default new Pool(config);
