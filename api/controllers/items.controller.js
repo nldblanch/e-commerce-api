c71a68bec5b1b2f86f997bfefe23d9f4a4fe6562
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-20 15:36:24 +0000

    update formatting

--- api/controllers/items.controller.js
+++ api/controllers/items.controller.js
@@ -1,30 +1,24 @@
 import { fetchCategoryFromSubcategory } from "../models/categories.model.js";
-import {
-  fetchAllItems,
-  fetchItem,
-  insertItem,
-  updateItem,
-  fetchUserItems
-} from "../models/items.model.js";
+import { fetchAllItems, fetchItem, insertItem, updateItem, fetchUserItems } from "../models/items.model.js";
 import { fetchUserByID } from "../models/users.model.js";
 import greenlist from "../utils/greenlist.js";
 import strictGreenlist from "../utils/strictGreenlist.js";
 
 const getAllItems = async (request, response, next) => {
-  const {query} = request;
-  const {sort_by, order} = query
+  const { query } = request;
+  const { sort_by, order } = query;
   try {
     if (sort_by) {
-      await greenlist(["name", "price", "date_listed"], [sort_by])
+      await greenlist(["name", "price", "date_listed"], [sort_by]);
     }
     if (order) {
-      await greenlist(["asc", "desc"], [order])
+      await greenlist(["asc", "desc"], [order]);
     }
     const items = await fetchAllItems(query);
     response.status(200).send({ items });
   } catch (error) {
     if (error.message === "bad request - invalid key or value") {
-      error.message = "invalid query parameter"
+      error.message = "invalid query parameter";
     }
     next(error);
   }
@@ -85,10 +79,10 @@ const patchItem = async (request, response, next) => {
       Object.keys(body)
     );
     await fetchUserByID(user_id);
-    const entries = Object.entries(body)
+    const entries = Object.entries(body);
     if (body.subcategory_id) {
-      const {category_id} = await fetchCategoryFromSubcategory(body.subcategory_id) 
-      entries.push(["category_id", category_id])
+      const { category_id } = await fetchCategoryFromSubcategory(body.subcategory_id);
+      entries.push(["category_id", category_id]);
     }
     const item = await updateItem(user_id, item_id, entries);
     response.status(200).send({ item });
@@ -98,14 +92,14 @@ const patchItem = async (request, response, next) => {
 };
 
 const getUserItems = async (request, response, next) => {
-    const { user_id } = request.params;
-    try {
-        await fetchUserByID(user_id);
-        const items = await fetchUserItems(user_id);
-        response.status(200).send({ items });
-      } catch (error) {
-        next(error);
-      }
-}
+  const { user_id } = request.params;
+  try {
+    await fetchUserByID(user_id);
+    const items = await fetchUserItems(user_id);
+    response.status(200).send({ items });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export { getAllItems, getItemByID, postItem, patchItem, getUserItems };
