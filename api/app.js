1a034244c4c2f6e36f49895b781604e7c04f0e46
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-20 17:20:51 +0000

    update formatting

--- api/app.js
+++ api/app.js
@@ -10,9 +10,7 @@ app.all("*", invalidEndpoint);
 
 app.use((error, request, response, next) => {
   if (error.code === "42703") {
-    response
-      .status(400)
-      .send({ message: "bad request - column does not exist" });
+    response.status(400).send({ message: "bad request - column does not exist" });
   } else {
     next(error);
   }
@@ -26,13 +24,11 @@ app.use((error, request, response, next) => {
 });
 app.use((error, request, response, next) => {
   if (error.code === "23502") {
-    response
-    .status(400)
-    .send({message: "bad request - missing key"})
+    response.status(400).send({ message: "bad request - missing key" });
   } else {
-    next(error)
+    next(error);
   }
-})
+});
 app.use((error, request, response, next) => {
   if (error.code && error.message) {
     response.status(error.code).send({ message: error.message });
