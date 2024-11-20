bbbbde3d9f74d2f5f72fb438736765f233eb5ed0
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-20 15:10:31 +0000

    update esling config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import prettier from 'eslint-plugin-prettier';
+import prettier from "eslint-plugin-prettier";
+import prettierRecommended from "eslint-config-prettier";
 export default [
   {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: 'module',
+      sourceType: "module",
     },
     plugins: {
       prettier,
     },
+    ...prettierRecommended,
     rules: {
-      'prettier/prettier': 'error',
-      semi: ['warn', 'always'],
+      "prettier/prettier": "error",
+      semi: ["warn", "always"],
     },
   },
 ];
