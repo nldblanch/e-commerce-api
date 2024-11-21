2794dc4a2da3a4deb07ecd82271312282b2cfe8c
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-21 14:18:07 +0000

    update formatting

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,7 +1,6 @@
-
 import data from "../data/test/index.js";
 import seed from "./seed.js";
-import db from "../connection.js"
+import db from "../connection.js";
 
 const runSeed = () => {
   return seed(data).then(() => db.end());
