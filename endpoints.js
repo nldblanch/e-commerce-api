9b74e26386f93cbadffe16dc8b94f41913a4155b
Nathan Blanch <nathan.blanch@northcoders.com>
2024-11-21 13:37:01 +0000

    update endpoints

--- endpoints.js
+++ endpoints.js
@@ -3,8 +3,7 @@ export default {
     id: 1,
     username: "tech_guru92",
     name: "John Doe",
-    avatar_url:
-      "https://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon",
+    avatar_url: "https://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon",
     data_registered: "2022-05-14T00:00:00.000Z",
     balance: 12050,
   },
@@ -17,10 +16,27 @@ export default {
     date_listed: "2023-04-30T23:00:00.000Z",
     available_item: true,
   },
+  example_order: {
+    id: 1,
+    seller_id: 1,
+    buyer_id: 2,
+    item_id: 1,
+    pending_order: true,
+    pending_feedback: true,
+    date_ordered: "2023-04-30T23:00:00.000Z",
+  },
+  example_category: {
+    id: 1,
+    category_name: "clothing_and_accessories",
+  },
+  example_subcategory: {
+    id: 1,
+    category_id: 1,
+    subcategory_name: "womens_clothing",
+  },
   "/api": {
     GET: {
-      description:
-        "serves up a json representation of all the available endpoints of the api",
+      description: "serves up a json representation of all the available endpoints of the api",
       exampleResponse: {
         endpoints: "/api/users",
       },
@@ -82,6 +98,17 @@ export default {
       },
     },
   },
+  "/api/users/:user_id/orders": {
+    GET: {
+      description: "gets all orders a user has made",
+      exampleResponse: { orders: "example_order" },
+    },
+    POST: {
+      description: "posts a new order representing purchasing an item",
+      format: { item_id: 1, seller_id: 1 },
+      exampleResponse: { order: "example_order" },
+    },
+  },
   "/api/users/user_id/items/:item_id": {
     PATCH: {
       description: "updates the data of an item belonging to a user",
@@ -96,11 +123,41 @@ export default {
       },
     },
   },
+  "/api/users/user_id/orders/:order_id": {
+    PATCH: {
+      description: "updates the order status of an order",
+      format: {
+        pending_order: false,
+      },
+      exampleResponse: {
+        order: "example_order",
+      },
+    },
+  },
   "/api/items": {
     GET: {
       description: "gets an array of items from the database",
-      exampleResponse: {
-        items: ["example_item", "example_item"],
+      queries: {
+        category: {
+          string: "category name",
+          number: "id",
+        },
+        subcategory: {
+          string: "subcategory name",
+          number: "id",
+        },
+        tag: { string: "key word" },
+        sort_by: {
+          options: ["name", "price", "date_listed"],
+          order: {
+            options: ["asc", "desc"],
+          },
+          price_to: { number: "maximum price" },
+          price_from: { number: "minimum price" },
+        },
+        exampleResponse: {
+          items: ["example_item", "example_item"],
+        },
       },
     },
   },
@@ -112,9 +169,25 @@ export default {
       },
     },
   },
-  "/api/orders": {
-    POST: {
-      description: "",
+  "/api/categories": {
+    GET: {
+      description: "gets all categories",
+      exampleResponse: {
+        item: "example_category",
+      },
+    },
+  },
+  "/api/categories/:category_id": {
+    GET: {
+      description: "gets all subcategories of the given category id",
+      exampleResponse: {
+        item: ["example_subcategory", "example_subcategory"],
+      },
+    },
+  },
+  "/api/orders/:order_id": {
+    GET: {
+      description: "gets an order based on the given id",
       exampleResponse: {
         order: "example_order",
       },
