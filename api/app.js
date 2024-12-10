773dbfb115191eaa0757a4332649fbb1251144f0
Nathan Blanch <nathan.blanch@northcoders.com>
2024-12-10 09:34:52 +0000

    allow cross origin resource sharing

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import apiRouter from "./routers/apiRouter.js";
 import { invalidEndpoint, customError, psqlErrors } from "./error-handling/index.js";
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
